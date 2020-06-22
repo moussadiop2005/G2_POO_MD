a0fb9b72d4beff04a12a324535ee6419e1b3f929
moussadiop2005 <moussadiop2005@hotmail.com>
2020-06-22 14:54:32 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Gestion de l’allocation des chambres</title>
+</head>
+<body>
+    <h1>Moussa Projet UML ,POO en PHP et MVC</h1>
+</body>
+</html>

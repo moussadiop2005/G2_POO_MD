17cd897788147b55bdcce5fbf85e18903020ff55
moussadiop2005 <62149726+moussadiop2005@users.noreply.github.com>
2020-06-25 18:51:26 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion de l’allocation des chambres</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+    <link rel="stylesheet" href="public/css/PageAccueil.css">
+</head>
+<body>
+    <div class="containerfluid header">
+        <div class="title">Gestion de chambre etudiants</div>
+    </div>
+    <div class="containerfluid body">
+
+        <div class="col-lg-6 menu rounded">
+            <div class="chambre">
+                <div class="logo-chambre "><img src="public/IMG/chambre.jpg" class="rounded"></div>
+                <div class="col-lg-7 lien-chambre">
+                    <a href="#"><div class="enregistrer">Enregistrer un chambre</div></a>
+                    <a href="#"><div class="lister">Liste des chambres</div></a>
+                </div>
+
+            </div>
+            <div class="etudiant">
+                <div class="logo-etudiant"><img src="public/IMG/etudiant.jpg" class="rounded"></div>
+                <div class="col-lg-7 lien-etudiant">
+                    <a href="#"><div class="enregistrer">Enregistrer un etudiant</div></a>
+                    <a href="#"><div class="lister">Liste des etudiants</div></a>
+                    
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ views/layouts/Layout.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion de l’allocation des chambres</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+    <link rel="stylesheet" href="../../public/css/Layout.css">
+</head>
+<body>
+    <div class="containerfluid header">
+        <div class="title">Gestion de chambre etudiants</div>
+    </div>
+    <div class="containerfluid body">
+
+        
+    </div>
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
